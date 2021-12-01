a12e17f987bdc9d9b15fa017a2b1e86022c93bfa
EM1LJAN <42141666+Emile1154@users.noreply.github.com>
2021-12-01 17:50:54 +0500

    work with request parameters & model

--- src/main/java/ru/emil/springcourseMVC/controllers/FirstController.java
+++ src/main/java/ru/emil/springcourseMVC/controllers/FirstController.java
@@ -1,14 +1,35 @@
 package ru.emil.springcourseMVC.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b", required = false) int b,
+                            @RequestParam(value = "o", required = false) String operation,
+                            Model model){
+        int result = 0;
+        if(operation.equals("sum")){
+            result = a+b;
+        }
+        if(operation.equals("multi")){
+            result = a*b;
+        }
+        if(operation.equals("minus")){
+            result = a-b;
+        }
+        if(operation.equals("div")){
+            result = a/b;
+        }
+        model.addAttribute("message",  "result: " + result);
         return "first/hello";
     }
     @GetMapping("/goodbye")
